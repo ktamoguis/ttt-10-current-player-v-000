1a4e24a802b587b813e99cdfc954cfd1be6d0113
Karl Amoguis <ktamoguis@gmail.com>
2018-04-19 16:24:26 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,13 @@
+def turn_count(board)
+  counter = 0
+  board.each do |turn|
+    if turn == "X" || turn == "O"
+      counter += 1
+    end
+  counter
+  end
+end
+
+def current_player(board)
+
+end
